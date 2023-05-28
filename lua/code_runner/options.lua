5a66425efb71457b12d81eb1d1c695d48259ad9a
Luca Saccarola <github.e41mv@aleeas.com>
2023-05-28 02:51:21 +0200

    feat: make defaults more sensible

--- lua/code_runner/options.lua
+++ lua/code_runner/options.lua
@@ -1,39 +1,24 @@
-local options = {
-    -- choose default mode (valid term, tab, float, toggle)
+local opt = {
     mode = "term",
-    -- Focus on runner window(only works on toggle, term and tab mode)
     focus = true,
-    -- startinsert (see ':h inserting-ex')
-    startinsert = false,
-    insert_prefix = "",
+    startinsert = true,
     term = {
-        --  Position to open the terminal, this option is ignored if mode ~= term
         position = "bot",
-        -- window size, this option is ignored if mode == tab
-        size = 12,
+        size = 15,
     },
     float = {
-        close_key = "<ESC>",
         -- Window border (see ':h nvim_open_win')
-        border = "none",
-
+        border = "rounded",
         -- Num from `0 - 1` for measurements
         height = 0.8,
         width = 0.8,
-        x = 0.5,
-        y = 0.5,
-
-        -- Highlight group for floating window/border (see ':h winhl')
-        border_hl = "FloatBorder",
-        float_hl = "Normal",
-
         -- Transparency (see ':h winblend')
         blend = 0,
     },
     before_run_filetype = function()
         vim.cmd(":w")
     end,
-    filetype = {
+    filetypes = {
         javascript = "node",
         java = "javac % && java %:t:r",
         c = "gcc % && %:h/a.out",
@@ -41,33 +26,22 @@ local options = {
         python = "python -u",
         sh = "bash",
         rust = "rustc % && %:p:r",
+        lua = ":luafile %",
+        vim = ":source %",
     },
-    project = {},
-    prefix = "",
+    projects = {},
 }
 
 local M = {}
 
-local function concat(v)
-    if type(v) == "table" then
-        return table.concat(v, " ")
-    end
-    return v
+---@param options table
+M.set = function(options)
+    opt = vim.tbl_deep_extend("force", opt, options)
 end
 
--- set user config
----@param user_options table
-M.set = function(user_options)
-    if user_options.startinsert then
-        user_options.insert_prefix = "startinsert"
-    end
-    options = vim.tbl_deep_extend("force", options, user_options)
-    options.filetype = vim.tbl_map(concat, options.filetype)
-    options.prefix = string.format("%s %d new", options.term.position, options.term.size)
-end
-
+---@return table
 M.get = function()
-    return options
+    return opt
 end
 
 return M
