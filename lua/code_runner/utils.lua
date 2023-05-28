4c9fde0983fa39bc45afe275b87d521d59cb60c9
Luca Saccarola <github.e41mv@aleeas.com>
2023-05-28 02:50:28 +0200

    feat: adding utils file

---
+++ lua/code_runner/utils.lua
@@ -0,0 +1,27 @@
+local M = {}
+
+---@param name string
+function M.buffer_rename(name)
+    if vim.fn.empty(name) ~= 1 then
+        vim.cmd("file " .. name)
+    end
+end
+
+---@param position string
+---@param size integer
+function M.term_create(position, size)
+    vim.cmd(string.format("%s %s new", position, size))
+end
+
+---@param command string
+---@return boolean
+function M.internal_command(command)
+    return command ~= nil and command:sub(1, 1) == ":"
+end
+
+---@param msg string
+function M.error(msg)
+    vim.notify("CODE RUNNER: " .. msg, vim.log.levels.ERROR)
+end
+
+return M
